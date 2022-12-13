a31590a798a0c4cd84ce126bfca74a9941a7897f
Adam Wozniak <adamwozhere@gmail.com>
2022-12-13 11:44:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// get form and add new button
+const form = document.getElementById('form');
+const btn = document.getElementById('banana-btn');
+
+// add a new bannana handler
+btn.addEventListener('click', () => {
+  form.insertAdjacentHTML('beforeend', createBanana());
+});
+
+function deleteHandler(banana) {
+  banana.closest('.banana').remove();
+}
+
+// make a new banana
+function createBanana() {
+  return `
+  <div class="banana">
+    <h3>This is a Banana</h3>
+    <p>This is a random banana number: ${randomNum()}</p>
+    <button class="delete-btn" onclick="deleteHandler(this)">delete</button>
+  </div>
+  `;
+}
+
+// random banana number
+function randomNum() {
+  return Math.floor(Math.random() * 100);
+}
