4a3311f5b7051c411d4c0bb5b7eb98010ed691cf
Adam Wozniak <adamwozhere@gmail.com>
2022-12-14 16:04:19 +0000

    can now drink PG Tips tea

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # banana-button
 
 Example for dynamically creating reusable items/components with a delete feature that functions without using ID's.
+(note: unique ID function added, but this isnt used for functionality - just an example so that it can be used as an ID for a `label` and `input` pair.)
+Banana's and Tea both use the same delete handler, as the handler targets the closest `data-item`.
 
 1. Add an item using a template literal, with a button inside.
 2. pass the clicked item to event handler `<button onclick="delete(this)">delete</button>`
--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 // get form and add new button
 const form = document.getElementById('form');
-const btn = document.getElementById('banana-btn');
+const bananaBtn = document.getElementById('banana-btn');
+const teaBtn = document.getElementById('tea-btn');
 
 // add a new bannana handler
-btn.addEventListener('click', () => {
+bananaBtn.addEventListener('click', () => {
   form.insertAdjacentHTML('beforeend', createBanana());
 });
 
-function deleteHandler(banana) {
-  banana.closest('.banana').remove();
+// add a new bannana handler
+teaBtn.addEventListener('click', () => {
+  form.insertAdjacentHTML('beforeend', createTea());
+});
+
+// handler takes in the delete button element as a parameter (passed in as 'this')
+// selects the closest selector and removes it, this could be a data attribute (put in [square brackets] or .class or #id etc.)
+function deleteHandler(element) {
+  element.closest('[data-item]').remove();
 }
 
+// cup of tea and bana both have 'data-item' attribute, which means they can be deleted
 // make a new banana
 function createBanana() {
   const uid = _UID();
   return `
-  <div class="banana" id="banana-${uid}">
+  <div class="banana" id="banana-${uid}" data-item>
     <h3>This is a Banana</h3>
     <p>&#127820;</p>
     <p>This is a random banana number: ${randomNum()}</p>
     <p>UID: ${uid}</p>
     <button class="delete-btn" onclick="deleteHandler(this)">Eat</button>
+  </div>
+  `;
+}
+
+// make a cup of tea
+function createTea() {
+  const uid = _UID();
+  return `
+  <div class="tea" id="tea-${uid}" data-item>
+    <h3>This is a cup of tea</h3>
+    <p>&#9749;</p>
+    <p>This is a random cup of tea number: ${randomNum()}</p>
+    <p>UID: ${uid}</p>
+    <button class="delete-btn" onclick="deleteHandler(this)">Drink</button>
   </div>
   `;
 }
